7550a0b98f22415efa0ad283fd3d6454ce624db4
yazoo1220 <podman1220@hotmail.com>
2023-05-20 22:59:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+
 from barfi import st_barfi, barfi_schemas, Block
 import streamlit as st
 
 from barfi import Block
 
+query = st.text_input('query')
+
 LLM = Block(name='LLM')
 LLM.add_input()
 LLM.add_input()
 LLM.add_output()
-def feed_func(self):
-    self.set_interface(name='Output 1', value=4)
-LLM.add_compute(feed_func)
+def get_llm(self):
+    llm = OpenAI(temperature=0.9)
+    self.set_interface(name='llm', value=4)
+LLM.add_compute(get_llm)
 
 Prompt = Block(name='Prompt')
 Prompt.add_output()
-def feed_func(self):
-    self.set_interface(name='Output 1', value=4)
-Prompt.add_compute(feed_func)
+def get_prompt(self):
+    prompt = PromptTemplate(
+        input_variables=["product"],
+        template="What is a good name for a company that makes {product}?",
+    )
+Prompt.add_compute(get_prompt)
 
 Memory = Block(name='Memory')
 Memory.add_output()
@@ -34,10 +44,10 @@ Chain.add_input()
 Chain.add_input()
 Chain.add_output()
 def mixer_func(self):
-    in_1 = self.get_interface(name='Input 1')
-    in_2 = self.get_interface(name='Input 2')
-    value = (in_1 + in_2)
-    self.set_interface(name='Output 1', value=value)
+    llm = self.get_interface(name='llm')
+    prompt = self.get_interface(name='prompt')
+    chain = LLMChain(llm=llm, prompt=prompt)
+    self.set_interface(name='Chain Output', value=chain.run({product:query}))
 Chain.add_compute(mixer_func)
 
 Agent = Block(name='Agent')
@@ -54,7 +64,8 @@ Agent.add_compute(mixer_func)
 result = Block(name='Result')
 result.add_input()
 def result_func(self):
-    in_1 = self.get_interface(name='Input 1')
+    result = self.get_interface(name='Input 1')
+    st.markdown(result)
 result.add_compute(result_func)
 
 load_schema = st.selectbox('Select a saved schema:', barfi_schemas())
