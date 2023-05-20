013b4639142ff1e4a11a31b3458b5ec15b46d37b
yazoo1220 <podman1220@hotmail.com>
2023-05-20 23:44:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -183,9 +183,9 @@ compute_engine = st.checkbox('Activate barfi compute engine', value=False)
 barfi_result = st_barfi(
     base_blocks=math_blocks, compute_engine=compute_engine, load_schema=load_schema)
 
-if barfi_result:
-    st.write(barfi_result['Result-id-524173']['block'].get_interface(name='Input 1'))
-    st.write(barfi_result)
+# if barfi_result:
+#     st.write(barfi_result['Result-id-524173']['block'].get_interface(name='Input 1'))
+#     st.write(barfi_result)
 
 if barfi_result:
     st.write(barfi_result)
