92b424b74368941e50a7244242e9425dbca7e9c2
yazoo1220 <podman1220@hotmail.com>
2023-05-20 14:08:41 +0300

    Update scenario_writer.py

--- scenario_writer.py
+++ scenario_writer.py
@@ -67,7 +67,7 @@ load_schema = st.selectbox('Select a saved schema:', barfi_schemas())
 
 compute_engine = st.checkbox('Activate barfi compute engine', value=False)
 
-barfi_result = st_barfi(base_blocks=[feed, result, mixer, splitter],
+barfi_result = st_barfi(base_blocks=[theme, outline, writing, validation, mixer, result],
                     compute_engine=compute_engine, load_schema=load_schema)
 
 if barfi_result:
