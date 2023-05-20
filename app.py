847f2579244c8e45c4f4eb1a62a6685683f1e654
yazoo1220 <podman1220@hotmail.com>
2023-05-20 23:32:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ barfi_result = st_barfi(base_blocks=[LLM, Prompt, Memory, VectorStore, Chain, Ag
 
 if barfi_result:
     st.write(barfi_result)
-    st.write(barfi_result)['Result-1']['block'].get_interface(name='Input 1'))
+    st.write(barfi_result)['Result-1']['block'].get_interface(name='Input 1')
